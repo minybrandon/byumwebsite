816f98f211e76d45dcf273137e53fe7d6b882aba
Brandon Yum <byum1996@gmail.com>
2019-12-06 22:55:10 -0800

    Start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Brandon Yum : Full-Stack Developer
         </p>
         <a
           className="App-link"
