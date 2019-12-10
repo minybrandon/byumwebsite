414987fcba11a8c3e8ec8532e59cc813d8aa7be0
Brandon Yum <byum1996@gmail.com>
2019-12-10 11:59:01 -0800

    Added top nav/tabs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SelfPortrait from '../src/Assets/selfportrait.svg';
+import '../src/Assets/selfportrait.css'
+import SimpleTabs from './Tabs.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+    
+    <div className="Tabs">
+      <SimpleTabs/>
+    </div>
+
+    <div className="Main">
+      <header className="Main-header">
+        <img alt='' src={SelfPortrait} />
         <p>
           Brandon Yum : Full-Stack Developer 
           Hello World!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nasa.gov/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          One small step for man.
         </a>
       </header>
     </div>
+    
+    </>
   );
 }
 
