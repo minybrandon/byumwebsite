f3c8b10b9c27385cf9bc49ae5596835c9e6eef68
Brandon Yum <byum1996@gmail.com>
2019-12-29 17:09:52 -0800

    Card elemnt

--- src/About.js
+++ src/About.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const About = () => {
-    return (
-        <Typography variant="subtitle1" gutterBottom>
-            Hi! My name is Brandon Yum. I am a self-taught full-stack web developer who lives in San Diego. 
-        </Typography>
-    )
-}
+const About = () => (
+    <Card className="AboutCard" variant="outlined">
+        <CardContent>
+            <Typography variant="h5" component="h2" gutterBottom>
+                   Hi! My name is Brandon Yum. I am a 23-year old currently and self-teaching myself full-stack web development. 
+                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Typography>
+        </CardContent>
+    </Card>
+);
 
 export default About;
