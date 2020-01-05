8300329371fca499790c982b36cd075f3b050f6e
Brandon Yum <byum1996@gmail.com>
2020-01-04 20:51:17 -0800

    About changes and more appealing tabs

--- src/About.js
+++ src/About.js
@@ -1,22 +1,17 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import SelfPortrait from './Assets/selfportrait.svg';
+
 
 const About = () => (
     <>
         <img alt="" src={SelfPortrait} />
-        <Card className="AboutCard" variant="outlined">
-            <CardContent>
-                <Typography variant="h5" component="h2" gutterBottom>
+        <h6>
                    Hi! My name is Brandon Yum. I am a 23-year old currently and self-teaching myself full-stack web development.
                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </Typography>
-            </CardContent>
-        </Card>
+        </h6>
+
     </>
 );
 
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
-const useStyles = makeStyles({
-    root: {
-        flexGrow: 1,
-    },
-});
-
 export default function CenteredTabs() {
-    const classes = useStyles();
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -20,19 +11,20 @@ export default function CenteredTabs() {
     };
 
     return (
-        <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="secondary"
-                centered
-            >
-                <Tab label="Home" component={Link} to="/" />
-                <Tab label="About" component={Link} to="/about" />
-                <Tab label="Projects" component={Link} to="/projects" />
-                <Tab label="My Music" component={Link} to="/mymusic" />
-            </Tabs>
-        </Paper>
+
+        <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="fullWidth"
+            indicatorColor="black"
+            textColor="primary"
+            centered
+        >
+            <Tab label="Home" component={Link} to="/" />
+            <Tab label="About" component={Link} to="/about" />
+            <Tab label="Projects" component={Link} to="/projects" />
+            <Tab label="My Music" component={Link} to="/mymusic" />
+        </Tabs>
+
     );
 }
